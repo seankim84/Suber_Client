3fee0bffb0e8c25849bc3f96d861b5a4b36b479c
SeanKim <sean.kim84@gmail.com>
2018-11-23 15:36:20 +0700

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        WelCome the FrontEnd
+      </div>
+    );
+  }
+}
+
+export default App;
