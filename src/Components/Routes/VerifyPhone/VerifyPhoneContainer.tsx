ac04c3255327d98ab7af6f6697da95219941df7a
SeanKim <sean.kim84@gmail.com>
2018-12-07 15:20:23 +0700

    VerifyPhone Mutation Part 1

--- src/Components/Routes/VerifyPhone/VerifyPhoneContainer.tsx
+++ src/Components/Routes/VerifyPhone/VerifyPhoneContainer.tsx
@@ -1,37 +1,55 @@
-
 import React from "react";
+import { Mutation } from "react-apollo";
 import { RouteComponentProps } from "react-router-dom";
+import { verifyPhone, verifyPhoneVariables } from "../../../types/api";
 import VerifyPhonePresenter from "./VerifyPhonePresenter";
+import { VERIFY_PHONE } from "./VerifyPhoneQueries";
 
 interface IState {
-    key: string;
+  key: string;
+  phoneNumber: string;
 }
 
-interface IProps extends RouteComponentProps<any> { }
+interface IProps extends RouteComponentProps<any> {}
+
+class VerifyMutation extends Mutation<verifyPhone, verifyPhoneVariables> {}
 
 class VerifyPhoneContainer extends React.Component<IProps, IState> {
-    public state = {
-        key: ""
+  constructor(props: IProps) {
+    super(props);
+    if (!props.location.state) {
+      props.history.push("/");
+    }
+    this.state = {
+      key: "",
+      phoneNumber: props.location.state.phone
     };
-    constructor(props: IProps) {
-        super(props);
-        if (!props.location.state) {
-            props.history.push("/");
-        }
-    }
-    public render() {
-        const { key } = this.state;
-        return <VerifyPhonePresenter onChange={this.onInputChange} key={key} />;
-    }
+  }
+  public render() {
+    const { key, phoneNumber } = this.state;
+    return (
+      <VerifyMutation
+        mutation={VERIFY_PHONE}
+        variables={{
+          key,
+          phoneNumber
+        }}
+      >
+        {(mutation, { loading }) => (
+          <VerifyPhonePresenter onChange={this.onInputChange} key={key} />
+        )}
+      </VerifyMutation>
+    );
+  }
 
-    public onInputChange: React.ChangeEventHandler<HTMLInputElement> = event => {
-        const {
-            target: { name, value }
-        } = event;
-        this.setState({
-            [name]: value
-        } as any);
-    };
+  public onInputChange: React.ChangeEventHandler<HTMLInputElement> = event => {
+    const {
+      target: { name, value }
+    } = event;
+    this.setState({
+      [name]: value
+    } as any);
+  };
 }
 
 export default VerifyPhoneContainer;
